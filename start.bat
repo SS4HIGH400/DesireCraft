e8f36baa256abe814c45e4f81e27f47ebcfc8c79
SS4HIGH400 <Dmitriy200713@gmail.com>
2026-05-21 22:48:48 +0300

    deisre
    adawd

--- start.bat
+++ start.bat
@@ -1,3 +1,6 @@
-chcp 65001 >nul
-java -Xms2G -Xmx4G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1ReservePercent=15 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://emc.gs -Daikars.new.flags=true -jar server.jar nogui
+@echo off
+cd /d "%~dp0"
+
+"C:\Program Files\Java\jdk-16.0.2+7\bin\java.exe" -Xmx4G -Xms4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar server.jar nogui
+
 pause
